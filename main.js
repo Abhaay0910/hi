b5f162baf8af616f9946f1e18926324f5a493705
Abhaay0910 <82942725+Abhaay0910@users.noreply.github.com>
2021-04-26 15:49:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log ("this is my first console project");
+var firstname="Abhaay";
+var lastname="Jhawar";
+var fullname=firstname.concat(lastname);
+console.log(fullname)
+var a=10;
+var b=15;
+var add=a+b;
+console.log(add)
+var sub=a-b;
+console.log(sub)
+var multiply=a*b;
+console.log (multiply)
+var divide=a/b;
+console.log(divide)
